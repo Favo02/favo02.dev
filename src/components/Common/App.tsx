af066ddc5d268351b88eb419e217c747683e11a4
Luca Favini <luca.favini.02@gmail.com>
2023-04-22 18:26:34 +0200

    feat(pages): add `ContentWrapper`

--- src/components/Common/App.tsx
+++ src/components/Common/App.tsx
@@ -1,11 +1,12 @@
 import { FC } from "react";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import ContentWrapper from "./ContentWrapper";
 import Home from "../../pages/Home";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <p>header</p>,
+    element: <ContentWrapper />,
     errorElement: <p>404</p>,
     children: [
       { path: "", element: <Home /> },
