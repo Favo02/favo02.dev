a8056ea2995ebf3e0567cd3bb720994def47e0c0
Luca Favini <luca.favini.02@gmail.com>
2023-04-23 15:17:01 +0200

    feat(css): add custom `bluegray` color

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}",],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        bluegray: {
+          50: "#f0f4f8",
+          100: "#d9e2ec",
+          200: "#bcccdc",
+          300: "#9fb3c8",
+          400: "#829ab1",
+          500: "#627d98",
+          600: "#486581",
+          700: "#334e68",
+          800: "#243b53",
+          900: "#102a43"
+        }
+      }
+    }
   },
-  plugins: [],
+  plugins: []
 }
