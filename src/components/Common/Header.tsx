1f2a5432b94b2faac80ddd048eab4cdd4c8be4a9
Luca Favini <luca.favini.02@gmail.com>
2023-04-24 17:39:44 +0200

    feat(components): add togglable `MenuButton` component
    add `MenuButton`: togglable `Header` menu button

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,15 +1,23 @@
-import { FC } from "react"
+import { FC, useState } from "react"
 import Logo from "../../assets/images/Logo.png"
+import MenuButton from "./MenuButton"
 
 const Header: FC = () => {
+
+  const [isOpen, setOpen] = useState<boolean>(false)
+
   return (
     <div className="w-full h-24 flex justify-center items-center fixed">
-      <div className="w-10/12 h-12 border-white bg-gray-400 bg-opacity-20 rounded-xl backdrop-blur-lg drop-shadow-l flex justify-between items-center">
+      <div className="w-10/12 max-w-5xl h-12 bg-gray-400 bg-opacity-20 rounded-xl backdrop-blur-lg drop-shadow-l flex justify-between items-center">
         <img src={Logo} className="ml-4 w-8 brightness-0 invert" />
-        <h1 className="mr-4 text-gray-300 font-normal tracking-[4px]">MENU</h1>
+        <div className="pr-4 pl-4 h-full cursor-pointer flex items-center" onClick={() => setOpen(!isOpen)}>
+          <h1 className="text-gray-300 font-normal tracking-[4px]">MENU</h1>
+          <MenuButton isOpen={isOpen} />
+        </div>
       </div>
     </div>
   )
 }
 
 export default Header
+
---
+++ src/components/Common/MenuButton.tsx
@@ -0,0 +1,16 @@
+import { FC } from "react"
+import "../../assets/styles/animations.css"
+
+const MenuButton: FC<{isOpen:boolean}> = ({ isOpen }) => {
+  return (
+    <div className="relative ml-2 w-10 h-10">
+      <div className={`absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 cursor-pointer ${isOpen ? "w-6" : "w-6"}`}>
+        <span className={`${isOpen ? "active" : "not-active"} first block w-full rounded-sm h-px bg-white transition-all relative`}></span>
+        <span className={`${isOpen ? "active" : "not-active"} second block w-full rounded-sm h-px bg-white transition-all relative mt-1`}></span>
+        <span className={`${isOpen ? "active" : "not-active"} third block w-full rounded-sm h-px bg-white transition-all relative mt-1`}></span>
+      </div>
+    </div>
+  )
+}
+
+export default MenuButton
