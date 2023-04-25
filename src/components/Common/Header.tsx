608a0a2bc2797709e2260821c8b10203fb309b34
Luca Favini <luca.favini.02@gmail.com>
2023-04-25 16:25:24 +0200

    fix(components): add `Link` to logo image in `Header`

--- src/components/Common/Header.tsx
+++ src/components/Common/Header.tsx
@@ -1,5 +1,6 @@
 import { FC, useState } from "react"
 import Logo from "../../assets/images/Logo.png"
+import { Link } from "react-router-dom"
 import MenuButton from "./MenuButton"
 import NavLink from "./NavLink"
 import "../../assets/styles/animations.css"
@@ -15,13 +16,15 @@ const Header: FC = () => {
         
         {/* Logo + Menu toggle */}
         <div className="flex justify-between items-center transition-all h-14">
-          <div className="flex justify-center items-center">
-            <img src={Logo} className="ml-4 w-8 brightness-0 invert" />
-            <div className="relative flex items-center w-28 h-14 overflow-hidden">
-              <div className="absolute slide-animation uppercase ml-3 font-light text-white z-1">Luca Favini</div>
-              <div className="absolute slide-animation-inverse uppercase ml-3 font-light text-white">favo02</div>
+          <Link to="/">
+            <div className="flex justify-center items-center">
+              <img src={Logo} className="ml-4 w-8 brightness-0 invert" />
+              <div className="relative flex items-center w-28 h-14 overflow-hidden">
+                <div className="absolute slide-animation uppercase ml-3 font-light text-white z-1">Luca Favini</div>
+                <div className="absolute slide-animation-inverse uppercase ml-3 font-light text-white">favo02</div>
+              </div>
             </div>
-          </div>
+          </Link>
 
           <div className="pr-4 pl-4 h-full cursor-pointer flex items-center" onClick={() => setOpen(!isOpen)}>
             <h1 className="text-gray-300 tracking-[4px]">MENU</h1>
