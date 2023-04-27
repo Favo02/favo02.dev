28433650b6cd7284b03b1bff377feb2c512185bf
Luca Favini <luca.favini.02@gmail.com>
2023-04-28 00:06:15 +0200

    feat(services): add `projects` service, `Repository` interface
    service endpoints:
     - `getAll`: return every (public) github repository

---
+++ src/interfaces/Repository.ts
@@ -0,0 +1,17 @@
+interface Repository {
+  description: string,
+  fork: boolean,
+  id: number,
+  language: string,
+  license: string,
+  name: string,
+  full_name: string,
+  private: boolean,
+  collaborators_url: string,
+  languages_url: string,
+  commits_url: string,
+  svn_url: string
+}
+
+
+export default Repository
---
+++ src/services/projects.ts
@@ -0,0 +1,18 @@
+import axios from "axios"
+
+// DO NOT use authorization, react does not permit to securely store keys (not even in .env)
+// backend needed to proxy requests
+const baseUrl = "https://api.github.com"
+
+const getAll = async () => {
+
+  const res = await axios.get(`${baseUrl}/users/Favo02/repos`)
+
+  return res.data
+}
+
+const projectsService = {
+  getAll
+}
+
+export default projectsService
