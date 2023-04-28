e5611d887d21142e47d5727caf3532b6ce40ddbc
Luca Favini <luca.favini.02@gmail.com>
2023-04-28 15:19:07 +0200

    feat(services): add `collaborators`, `languages` endpoints
    add `collaborators`, `languages` endpoints in `repository` service

--- src/services/repositories.ts
+++ src/services/repositories.ts
@@ -9,8 +9,20 @@ const getAll = async () => {
   return res.data
 }
 
+const collaborators = async (repo: string) => {
+  const res = await axios.get(`${baseUrl}/repos/${repo}/contributors`)
+  return res.data
+}
+
+const languages = async (repo: string) => {
+  const res = await axios.get(`${baseUrl}/repos/${repo}/languages`)
+  return res.data
+}
+
 const repositoriesService = {
-  getAll
+  getAll,
+  collaborators,
+  languages
 }
 
 export default repositoriesService
