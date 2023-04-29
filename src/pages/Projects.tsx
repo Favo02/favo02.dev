965d76124514b67ef68bb3266d01fb2071acdfde
Luca Favini <luca.favini.02@gmail.com>
2023-04-29 21:11:31 +0200

    refactor(css): change text color in `Projects` page

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -6,7 +6,7 @@ const Projects: FC = () => {
   return (
     <div className="pt-36 w-10/12 max-w-6xl m-auto">
       <div>
-        <h1 className="text-bluegray-100 text-center text-4xl font-mono tracking-tighter"><span className="text-bluegray-500">/</span>projects</h1>
+        <h1 className="text-gray-100 text-center text-4xl font-mono tracking-tighter"><span className="text-bluegray-400">/</span>projects</h1>
         <h4 className="mt-4 text-2xl text-gray-200 text-center">Every <span className="font-bold bg-gradient-to-r from-gray-400 via-bluegray-600 to-transparent bg-no-repeat bg-[size:100%_15%] bg-[position:0_98%]">project</span> I have worked on.</h4>
       </div>
 
