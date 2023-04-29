39530174c51d4c2c6483b89f8a81ab855461a0ec
Luca Favini <luca.favini.02@gmail.com>
2023-04-29 19:31:35 +0200

    feat(components): add `ProjectsList`
    `ProjectsList`: component to display all projects fetching github api

--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -1,14 +1,22 @@
 import { FC } from "react"
+import { Link } from "react-router-dom"
+import ProjectsList from "../components/Projects/ProjectsList"
 
 const Projects: FC = () => {
   return (
-    <div className="pt-36">
+    <div className="pt-36 w-10/12 max-w-6xl m-auto">
       <div>
-        <h1 className="text-gray-200 text-center text-3xl font-bold">Featured <span className="uppercase font-light bg-gradient-to-r from-gray-400 via-bluegray-600 to-transparent bg-no-repeat bg-[size:100%_20%] bg-[position:0_98%]">projects</span></h1>
+        <h1 className="text-bluegray-100 text-center text-4xl font-mono tracking-tighter"><span className="text-bluegray-500">/</span>projects</h1>
+        <h4 className="mt-4 text-2xl text-gray-200 text-center">Every <span className="font-bold bg-gradient-to-r from-gray-400 via-bluegray-600 to-transparent bg-no-repeat bg-[size:100%_15%] bg-[position:0_98%]">project</span> I have worked on.</h4>
       </div>
 
-      <div>
-        <h1 className="text-gray-200 text-center text-3xl font-bold">Other <span className="uppercase font-light bg-gradient-to-r from-gray-400 via-bluegray-600 to-transparent bg-no-repeat bg-[size:100%_20%] bg-[position:0_98%]">projects</span></h1>
+      <ProjectsList />
+
+      <div className="text-gray-500 text-center mt-10">
+        <div>This page uses <Link to="https://docs.github.com/en/rest" target="_blank" rel="noopener noreferrer" className="underline">Github v3 REST API</Link>.</div>
+
+        <div className="italic mt-2">This page does not include private projects.</div>
+        <div className="italic">This page is work in progress.</div>
       </div>
     </div>
   )
