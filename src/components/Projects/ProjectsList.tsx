cac06424703f50f0b1c23469a5ac08abdf49456b
Luca Favini <luca.favini.02@gmail.com>
2023-04-30 13:51:29 +0200

    fix(components): add loading in `ProjectsList`
    add `Loading` during API calls in `ProjectsList`

--- src/components/Projects/ProjectsList.tsx
+++ src/components/Projects/ProjectsList.tsx
@@ -2,19 +2,31 @@ import { FC, useState, useEffect } from "react"
 import repositoriesService from "../../services/repositories"
 import Repository from "../../interfaces/Repository"
 import ProjectCard from "./ProjectCard"
+import Loading from "../Common/Loading"
 
 const Projects : FC = () => {
 
+  const [loading, setLoading] = useState<boolean>(true)
   const [repositories, setRepositories] = useState<Repository[]>()
 
   useEffect(() => {
     const fetch = async () => {
+      setLoading(true)
       const repos = await repositoriesService.getAll()
       setRepositories(repos)
+      setLoading(false)
     }
 
     fetch()
   }, [])
+
+  if (loading) {
+    return (
+      <div className="w-full h-full min-h-[calc(100vh-300px)] flex justify-center items-center">
+        <Loading />
+      </div>
+    )
+  }
 
   return (
     <div className="flex flex-wrap justify-center mt-6">
