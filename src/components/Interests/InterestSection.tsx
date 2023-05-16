2ac015a7fa3782e2e66eff5a740088bab7f2bf6c
Luca Favini <luca.favini.02@gmail.com>
2023-05-16 17:16:50 +0200

    fix(css): add hover effect on `InterestSection

--- src/components/Interests/InterestSection.tsx
+++ src/components/Interests/InterestSection.tsx
@@ -13,12 +13,12 @@ interface props {
 const InterestSection : FC<{interests : props[]}> = ({ interests }) => (
   <>
     {interests.map(i =>
-      <div className="relative flex flex-col w-full py-10 text-center overflow-hidden group opacity-80 hover:opacity-100 transition-all duration-700" key={i.title}>
+      <div className="relative flex flex-col w-full py-10 text-center overflow-hidden group hover:py-16 transition-all duration-700" key={i.title}>
         <h2 className="text-2xl uppercase font-bold">
-          <span className="bg-gradient-to-r from-gray-400 to-bluegray-600 bg-no-repeat bg-[size:80%_30%] bg-[position:50%_97%]">{i.title}</span>
+          <span className="bg-gradient-to-r from-gray-400 to-bluegray-600 bg-no-repeat bg-[size:80%_30%] bg-[position:50%_97%] group-hover:bg-[size:100%_30%] group-hover:bg-[position:0%_97%] transition-all duration-700">{i.title}</span>
         </h2>
 
-        <p className="text-xl w-7/12 m-auto my-6">{i.description}</p>
+        <p className="text-xl w-7/12 m-auto my-6 text-gray-400 group-hover:text-gray-200 transition-all duration-700">{i.description}</p>
         <p className="font-mono text-gray-500">{i.languages}</p>
 
         <ScrollingIcons repeat={i.iconsRepeat} delay={false} position="left-3">
