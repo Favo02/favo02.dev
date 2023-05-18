e47c6367a8904c7dd1b9b7e5add5acf5fcb4eca9
Luca Favini <luca.favini.02@gmail.com>
2023-05-19 00:19:21 +0200

    feat(pages): add example projects in `interests` page

--- src/components/Interests/InterestSection.tsx
+++ src/components/Interests/InterestSection.tsx
@@ -1,5 +1,6 @@
 import type { FC } from "react"
 
+import InterestProjectExample from "./InterestProjectExample"
 import ScrollingIcons from "./ScrollingIcons"
 
 interface props {
@@ -7,19 +8,24 @@ interface props {
   description : string,
   languages : string,
   icons : React.ReactNode[],
-  iconsRepeat : number
+  iconsRepeat : number,
+  projects : string[]
 }
 
 const InterestSection : FC<{interests : props[]}> = ({ interests }) => (
   <>
     {interests.map(i =>
-      <div className="relative flex flex-col w-full py-10 text-center overflow-hidden group hover:py-16 transition-all duration-700" key={i.title}>
+      <div className="relative flex flex-col w-full py-10 text-center overflow-hidden group transition-all duration-700" key={i.title}>
         <h2 className="text-2xl uppercase font-bold">
           <span className="bg-gradient-to-r from-gray-400 to-bluegray-600 bg-no-repeat bg-[size:80%_30%] bg-[position:50%_97%] group-hover:bg-[size:100%_30%] group-hover:bg-[position:0%_97%] transition-all duration-700">{i.title}</span>
         </h2>
 
         <p className="text-xl w-7/12 m-auto my-6 text-gray-400 group-hover:text-gray-200 transition-all duration-700">{i.description}</p>
         <p className="font-mono text-gray-500">{i.languages}</p>
+
+        <div className="opacity-0 h-0 group-hover:h-80 group-hover:opacity-100 transition-all duration-700 flex justify-center">
+          {i.projects.map(p => <InterestProjectExample key={p} name={p} />)}
+        </div>
 
         <ScrollingIcons repeat={i.iconsRepeat} delay={false} position="left-3">
           {i.icons}
