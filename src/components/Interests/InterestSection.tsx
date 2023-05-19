11344c41aae64a4b86b2a85a7f15e0213d9b3fd8
Luca Favini <luca.favini.02@gmail.com>
2023-05-19 16:58:15 +0200

    fix(css): replace `ScrollingIcons` with `LateralIcons`
    in `interests` page

--- src/components/Interests/InterestSection.tsx
+++ src/components/Interests/InterestSection.tsx
@@ -1,52 +1,66 @@
 import type { FC } from "react"
+import React, { useEffect, useRef,useState } from "react"
 import { Link } from "react-router-dom"
 
 import InterestProjectExample from "./InterestProjectExample"
-import ScrollingIcons from "./ScrollingIcons"
+import LateralIcons from "./LateralIcons"
 
 interface props {
   title : string,
   description : string,
   languages : string,
   icons : React.ReactNode[],
-  iconsRepeat : number,
   projects : string[]
 }
 
-const InterestSection : FC<{interests : props[]}> = ({ interests }) => (
-  <>
-    {interests.map(i =>
-      <div className="relative flex flex-col w-full py-10 text-center overflow-hidden group transition-all duration-700" key={i.title}>
-        <h2 className="text-2xl uppercase font-bold">
-          <span className="bg-gradient-to-r from-gray-400 to-bluegray-600 bg-no-repeat bg-[size:80%_30%] bg-[position:50%_97%] group-hover:bg-[size:100%_30%] group-hover:bg-[position:0%_97%] transition-all duration-700">{i.title}</span>
-        </h2>
+const InterestSection : FC<{interests : props[]}> = ({ interests }) => {
 
-        <p className="text-xl w-7/12 m-auto my-6 text-gray-400 group-hover:text-gray-200 transition-all duration-700">{i.description}</p>
-        <p className="font-mono text-gray-500">{i.languages}</p>
+  const [height, setHeight] = useState<number | undefined>(0)
+  const ref = useRef<HTMLInputElement>(null)
 
-        <div className="opacity-0 h-0 group-hover:h-[22rem] group-hover:opacity-100 transition-all duration-700">
-          <div className="flex justify-center">
-            {i.projects.map(p => <InterestProjectExample key={p} name={p} />)}
+  useEffect(() => {
+    if (ref) {
+      setHeight(ref.current?.clientHeight)
+    }
+  }, [])
+
+  return (
+    <>
+      {interests.map(i =>
+        <div
+          ref={ref} key={i.title}
+          className="relative flex flex-col w-full py-10 text-center overflow-hidden group transition-all duration-700">
+          <h2 className="text-2xl uppercase font-bold">
+            <span className="bg-gradient-to-r from-gray-400 to-bluegray-600 bg-no-repeat bg-[size:80%_30%] bg-[position:50%_97%] group-hover:bg-[size:100%_30%] group-hover:bg-[position:0%_97%] transition-all duration-700">{i.title}</span>
+          </h2>
+
+          <p className="text-xl w-7/12 m-auto my-6 text-gray-400 group-hover:text-gray-200 transition-all duration-700">{i.description}</p>
+          <p className="font-mono text-gray-500">{i.languages}</p>
+
+          <div className="opacity-0 h-0 group-hover:h-[22rem] group-hover:opacity-100 transition-all duration-700">
+            <div className="flex justify-center">
+              {i.projects.map(p => <InterestProjectExample key={p} name={p} />)}
+            </div>
+            <p className="italic text-gray-600">
+              And more... Visit
+              <Link to="/projects">
+                <span className="font-mono text-gray-400 tracking-tighter mx-2">/projects</span>
+              </Link>
+              page to view all projects.
+            </p>
           </div>
-          <p className="italic text-gray-600">
-            And more... Visit
-            <Link to="/projects">
-              <span className="font-mono text-gray-400 tracking-tighter mx-2">/projects</span>
-            </Link>
-            page to view all projects.
-          </p>
+
+          <LateralIcons position="left-3" height={height ?? 0}>
+            {i.icons}
+          </LateralIcons>
+
+          <LateralIcons position="right-3" height={height ?? 0}>
+            {i.icons}
+          </LateralIcons>
         </div>
-
-        <ScrollingIcons repeat={i.iconsRepeat} delay={false} position="left-3">
-          {i.icons}
-        </ScrollingIcons>
-
-        <ScrollingIcons repeat={i.iconsRepeat} delay={true} position="right-3">
-          {i.icons}
-        </ScrollingIcons>
-      </div>
-    )}
-  </>
-)
+      )}
+    </>
+  )
+}
 
 export default InterestSection
